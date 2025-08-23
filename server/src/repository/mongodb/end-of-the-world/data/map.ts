95255034fe7df5c2bf93f5256d525ef01fcf05c1
brs_script <barko243@gmail.com>
2025-08-23 16:21:04 +0300

    map.ts yapılıyor.

--- server/src/repository/mongodb/end-of-the-world/data/map.ts
+++ server/src/repository/mongodb/end-of-the-world/data/map.ts
@@ -1,3 +1,46 @@
+import {
+  AK47,
+  Bandage,
+  bulletItems,
+  CargoPants,
+  Colt1911,
+  containerItems,
+  DiamondEarring,
+  foodItems,
+  GoldRing,
+  HeavyMilitaryHelmet,
+  Jacket,
+  LightMilitaryHelmet,
+  LouisvilleSlugger,
+  LuxuryWatch,
+  M4A1,
+  Machete,
+  Meat,
+  medicalItems,
+  MilitaryBoots,
+  MilitaryJacket,
+  MotorcycleHelmet,
+  OldPainting,
+  Pants,
+  PistolBullet,
+  PumpgunBullet,
+  Remington870,
+  RifleBullet,
+  RottenMeat,
+  Shorts,
+  SilverNecklace,
+  Sneakers,
+  StanleyCrowbar,
+  Sweater,
+  TShirt,
+  Water,
+  weaponItems,
+  type Item,
+} from "./items";
+
+type Effect = "very-low" | "low" | "medium" | "high" | "very-high";
+
+// *************************** Region ***************************
 export enum Regions {
   North = "Kuzey",
   South = "Güney",
@@ -5,8 +48,151 @@ export enum Regions {
   East = "Doğu",
 }
 
-type Effect = "very-low" | "low" | "medium" | "high" | "very-high";
+enum RegionEffects {
+  Cold = "Cold",
+  Hot = "Hot",
+  Normal = "Normal",
+  HuntingOpportunity = "HuntingOpportunity", // av fırsatı
+  WildAnimalThreat = "WildAnimalThreat",
+}
 
+export type RegionEffect = {
+  regionEffect: RegionEffects;
+  description: string;
+  effect: Effect;
+};
+
+const Cold = (effect: Effect): RegionEffect => {
+  return {
+    regionEffect: RegionEffects.Cold,
+    description: "Soğuk",
+    effect: effect,
+  } as RegionEffect;
+};
+
+const Hot = (effect: Effect): RegionEffect => {
+  return {
+    regionEffect: RegionEffects.Hot,
+    description: "Sıcak",
+    effect: effect,
+  } as RegionEffect;
+};
+
+const Normal = (effect: Effect): RegionEffect => {
+  return {
+    regionEffect: RegionEffects.Normal,
+    description: "Normal",
+    effect: effect,
+  } as RegionEffect;
+};
+
+const HuntingOpportunity = (effect: Effect): RegionEffect => {
+  return {
+    regionEffect: RegionEffects.HuntingOpportunity,
+    description: "Av Fırsatı",
+    effect: effect,
+  } as RegionEffect;
+};
+
+const WildAnimalThreat = (effect: Effect): RegionEffect => {
+  return {
+    regionEffect: RegionEffects.WildAnimalThreat,
+    description: "Vahşi Hayvan Tehdidi",
+  } as RegionEffect;
+};
+
+export type Region = {
+  region: Regions;
+  difficulty: "easy" | "medium" | "hard";
+  items: Array<Item>;
+  effects: Array<RegionEffect>;
+};
+
+export const North: Region = {
+  region: Regions.North,
+  difficulty: "hard",
+  items: [
+    ...foodItems,
+    Sweater,
+    Jacket,
+    MilitaryJacket,
+    Pants,
+    CargoPants,
+    MilitaryBoots,
+    LightMilitaryHelmet,
+    HeavyMilitaryHelmet,
+    ...weaponItems,
+    ...containerItems,
+    ...bulletItems,
+    ...medicalItems,
+  ],
+  effects: [Cold("very-high"), HuntingOpportunity("high"), WildAnimalThreat("high")],
+} as Region;
+
+export const South: Region = {
+  region: Regions.South,
+  difficulty: "medium",
+  items: [
+    Water,
+    Meat,
+    RottenMeat,
+    TShirt,
+    Shorts,
+    MotorcycleHelmet,
+    Colt1911,
+    AK47,
+    Remington870,
+    Machete,
+    LouisvilleSlugger,
+    StanleyCrowbar,
+    ...containerItems,
+    PistolBullet,
+    RifleBullet,
+    PumpgunBullet,
+    GoldRing,
+    SilverNecklace,
+    DiamondEarring,
+    Bandage,
+  ],
+  effects: [Hot("very-high"), HuntingOpportunity("medium"), WildAnimalThreat("high")],
+} as Region;
+
+export const West: Region = {
+  region: Regions.West,
+  difficulty: "medium",
+  items: [
+    ...foodItems,
+    TShirt,
+    Shorts,
+    Pants,
+    Sneakers,
+    Colt1911,
+    M4A1,
+    Remington870,
+    Machete,
+    LouisvilleSlugger,
+    StanleyCrowbar,
+    ...containerItems,
+    PistolBullet,
+    RifleBullet,
+    PumpgunBullet,
+    LuxuryWatch,
+    OldPainting,
+    ...medicalItems,
+  ],
+  effects: [Normal("very-high"), HuntingOpportunity("low")],
+} as Region;
+
+export const East: Region = {
+  region: Regions.East,
+  difficulty: "medium",
+  items: [],
+  effects: [Hot("medium"), HuntingOpportunity("medium"), WildAnimalThreat("high")],
+} as Region;
+
+// *************************** Region ***************************
+
+// *************************** Area ***************************
 enum AreaEffects {}
 
 type AreaEffect = {
@@ -16,9 +202,11 @@ type AreaEffect = {
 };
 
 export type Area = {
-  region: Regions;
+  region: Region;
   locations: Array<Location>;
-  effects: any;
+  effects: AreaEffect;
 };
+
+// *************************** Area ***************************
 
 export type Location = {};
